d66b07898ba25f3dfab5e2ac6a0cc60abb4ec9b6
Willi'a Hardy <hardywillia@outlook.com>
2018-11-07 23:22:25 -0500

    initial commit of project

---
+++ PhoenixJukebox/FirstPage.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PhoenixJukebox
+{
+    partial class FirstPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.AutoSize = true;
+            this.btnConnect.Location = new System.Drawing.Point(163, 128);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(171, 30);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect to Database";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FirstPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 780);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "FirstPage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FirstPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ PhoenixJukebox/FirstPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PhoenixJukebox
+{
+    public partial class FirstPage : Form
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = "server = localhost; user id = root; password = prAc7ice2018!;";
+        private void FirstPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try{
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    if (con.State == System.Data.ConnectionState.Closed)
+                        con.Open();
+                    MessageBox.Show("Connection opened successfully.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+          
+        }
+    }
+}
